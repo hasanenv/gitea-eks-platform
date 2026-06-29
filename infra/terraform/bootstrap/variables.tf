da898efd8b65b2f8d027b2d1c6455f5e82318ff9
Hasan <hasan-kamran@outlook.com>
2026-06-29 21:30:34 +0100

    feat: add Terraform bootstrap with S3 remote state backend

---
+++ infra/terraform/bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "owner" {
+  type    = string
+  default = "hasan"
+}
+
+variable "project_name" {
+  type    = string
+  default = "gitea-eks-platform"
+}
