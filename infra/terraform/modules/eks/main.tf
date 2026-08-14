76be23d4a6a2a8da61b0134713d88024e0538d7e
Hasan Kamran <hasan-kamran@outlook.com>
2026-08-14 16:04:00 +0100

    feat: add KMS encryption for EKS secrets at rest

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -2,7 +2,6 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-eks-cluster-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -15,7 +14,6 @@ resource "aws_iam_role" "eks_cluster_role" {
       },
     ]
   })
-
   tags = local.common_tags
 }
 
@@ -24,14 +22,27 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource "aws_kms_key" "eks_secrets" {
+  description             = "EKS secrets encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  tags                    = local.common_tags
+}
+
 resource "aws_eks_cluster" "eks_cluster" {
   #checkov:skip=CKV_AWS_39: Public endpoint intentionally enabled with CIDR restriction to known IPs
-  #checkov:skip=CKV_AWS_58: KMS encryption requires cluster recreation - planned for next rebuild
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
   version  = "1.34"
 
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.eks_secrets.arn
+    }
+    resources = ["secrets"]
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_role_attachment
@@ -49,7 +60,6 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 resource "aws_iam_role" "eks_node_group_role" {
   name = "${var.cluster_name}-eks-node-group-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -62,23 +72,22 @@ resource "aws_iam_role" "eks_node_group_role" {
       },
     ]
   })
-
   tags = local.common_tags
 }
 
 resource "aws_iam_role_policy_attachment" "eks_node_group_role_attachment" {
   role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy" # used for EKS worker nodes to join the cluster
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
   role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy" # used for the Amazon VPC CNI plugin for Kubernetes
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
   role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly" # used for pulling container images from ECR
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_eks_node_group" "eks_node_group" {
@@ -86,6 +95,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = var.private_subnets
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_node_group_role_attachment,
     aws_iam_role_policy_attachment.eks_cni_policy_attachment,
@@ -100,22 +110,19 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   instance_types = var.instance_types
   capacity_type  = var.capacity_type
-
-  tags = local.common_tags
+  tags           = local.common_tags
 }
 
 resource "aws_iam_openid_connect_provider" "eks_oidc" {
   url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["06b25927c42a721631c1efd9431e648fa62e1e39"]
-
-  tags = local.common_tags
+  tags            = local.common_tags
 }
 
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name             = aws_eks_cluster.eks_cluster.name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = var.ebs_csi_role_arn
-
-  depends_on = [aws_eks_node_group.eks_node_group]
+  depends_on               = [aws_eks_node_group.eks_node_group]
 }
